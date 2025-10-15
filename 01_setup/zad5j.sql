5a33b7801402422b4b74855d7d860d8e95b6176e
olafraczek <117641617+olafraczek@users.noreply.github.com>
2025-10-15 16:59:14 +0200

    Create zad5j.sql

---
+++ 01_setup/zad5j.sql
@@ -0,0 +1,6 @@
+SELECT p.imie, p.nazwisko, pe.kwota AS pensja, pr.kwota AS premia
+FROM ksiegowosc.pracownicy p
+JOIN ksiegowosc.wynagrodzenie w ON p.id_pracownika = w.id_pracownika
+JOIN ksiegowosc.pensja pe ON w.id_pensji = pe.id_pensji
+JOIN ksiegowosc.premia pr ON w.id_premii = pr.id_premii
+ORDER BY pe.kwota DESC, pr.kwota DESC NULLS LAST;
